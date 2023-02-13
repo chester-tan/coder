70f244b62c8edab8a8809795738b206c69bc8689
chester-tan <mail@chester-tan.com>
2023-02-13 10:33:52 +0100

    hello

--- coder.sh
+++ coder.sh
@@ -5,12 +5,7 @@ export DOCKER_GROUP=$(getent group docker | cut -d: -f3)
 mkdir -p $CODER_DATA
 docker run --rm -it \
     -v $CODER_DATA:/home/coder/.config \
+    -e CODER_DERP_CONFIG_URL="https://controlplane.tailscale.com/derpmap/default" \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --group-add $DOCKER_GROUP \
     ghcr.io/coder/coder:latest
-# docker run --rm -it \
-#     -v $CODER_DATA:/home/coder/.config \
-#     -e CODER_DERP_CONFIG_URL="https://controlplane.tailscale.com/derpmap/default" \
-#     -v /var/run/docker.sock:/var/run/docker.sock \
-#     --group-add $DOCKER_GROUP \
-#     ghcr.io/coder/coder:latest
