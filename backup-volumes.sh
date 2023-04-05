f1c2b731379f6607d5700755c5f3f3100a51894a
chester-tan <mail@chester-tan.com>
2023-04-05 20:20:22 +0200

    hello

--- backup-volumes.sh
+++ backup-volumes.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Get all docker volumes
-volumes=$(docker volume ls -q)
+BACKUP_DIR="/mnt/c/Users/ml4nets/Documents/docker-volume-backup"
 
-# Iterate through each volume
-for volume in $volumes
-do
-  # Create a backup folder
-  mkdir -p /backups/$volume
+# Create backup directory if it doesn't exist
+if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir "$BACKUP_DIR"
+fi
 
-  # Backup the volume
-  docker run --rm \
-    -v $volume:/data \
-    -v /backups/$volume:/backup \
-    alpine \
-    tar cvf /backup/volume_backup.tar /data
+# Backup each volume
+for volume_name in $(sudo docker volume ls --quiet); do
+    echo "Backing up volume $volume_name ..."
+    sudo docker run --rm \
+        -v "$volume_name":/source \
+        -v "$BACKUP_DIR":/backup \
+        alpine tar -czvf "/backup/$volume_name.tar.gz" -C /source .
 done
+
+echo "All volumes backed up to $BACKUP_DIR"
