4a9ff5df6e0d985a67221b38cda6a3b86904ffd4
chester-tan <mail@chester-tan.com>
2023-04-18 14:05:03 +0200

    hello

--- backup-volumes.sh
+++ backup-volumes.sh
@@ -8,21 +8,21 @@ if [ ! -d "$BACKUP_DIR" ]; then
 fi
 
 # Backup (and compress) each volume
-# for volume_name in $(sudo docker volume ls --quiet); do
-#     echo "Backing up volume $volume_name ..."
-#     sudo docker run --rm \
-#         -v "$volume_name":/source \
-#         -v "$BACKUP_DIR":/backup \
-#         alpine tar -czvf "/backup/$volume_name.tar.gz" -C /source .
-# done
-
-# Backup (without compressing) each volume
 for volume_name in $(sudo docker volume ls --quiet); do
     echo "Backing up volume $volume_name ..."
     sudo docker run --rm \
         -v "$volume_name":/source \
         -v "$BACKUP_DIR":/backup \
-        alpine tar -cvf "/backup/$volume_name.tar" -C /source .
+        alpine tar -czvf "/backup/$volume_name.tar.gz" -C /source .
 done
+
+# # Backup (without compressing) each volume
+# for volume_name in $(sudo docker volume ls --quiet); do
+#     echo "Backing up volume $volume_name ..."
+#     sudo docker run --rm \
+#         -v "$volume_name":/source \
+#         -v "$BACKUP_DIR":/backup \
+#         alpine tar -cvf "/backup/$volume_name.tar" -C /source .
+# done
 
 echo "All volumes backed up to $BACKUP_DIR"
