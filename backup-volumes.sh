8a6fa0979bb6867792939fb7ddb0607f248ce7e4
chester-tan <mail@chester-tan.com>
2023-04-18 13:46:23 +0200

    hello

--- backup-volumes.sh
+++ backup-volumes.sh
@@ -22,7 +22,7 @@ for volume_name in $(sudo docker volume ls --quiet); do
     sudo docker run --rm \
         -v "$volume_name":/source \
         -v "$BACKUP_DIR":/backup \
-        alpine cp -R /backup /source
+        alpine tar -cvf "/backup/$volume_name.tar" -C /source .
 done
 
 echo "All volumes backed up to $BACKUP_DIR"
